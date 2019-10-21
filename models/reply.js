bae554655341e4f446260c5640135f9a0c2c815d
matekuor <matekuor94@gmail.com>
2019-10-21 07:40:37 +0100

    first commit

---
+++ models/reply.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+var replySchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: 'Description can\'t be empty'
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    issue_id: {
+        type: String,
+        required: 'Issue Id can\'t be empty'
+    },
+    commenter: {
+        type: String,
+        required: 'Commenter can\'t be empty'
+    },
+});
+
+const Reply = mongoose.model('Reply', replySchema);
+module.exports = { Reply }
