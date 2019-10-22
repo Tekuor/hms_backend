5d1d1299d507637320d3d33b924f2a1018654bcf
matekuor <matekuor94@gmail.com>
2019-10-22 15:11:11 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,32 +1,7 @@
 let jwt = require("jsonwebtoken");
-let config = require("./index.js");
-const SECRET_KEY = "secretkey23456";
+let config = require("./config.js");
 
 let checkToken = (req, res, next) => {
-  //let token = req.headers['x-access-token'] || req.headers['authorization'];
-  //   let token = req.headers.authorization.split(" ")[1];
-
-  //   console.log(token);
-
-  // if (token) {
-
-  //     jwt.verify(token, config.SECRET_KEY, (err, decoded) => {
-  //     if (err) {
-  //         return res.json({
-  //         success: false,
-  //         message: 'Token is not valid'
-  //         });
-  //     } else {
-  //         req.decoded = decoded;
-  //         next();
-  //     }
-  //     });
-  // } else {
-  //     return res.json({
-  //     success: false,
-  //     message: 'Auth token is not supplied'
-  //     });
-  // }
 
   const authorizationHeader = req.headers.authorization;
   if (authorizationHeader) {
@@ -37,7 +12,7 @@ let checkToken = (req, res, next) => {
       issuer: "gifty"
     };
     try {
-      decoded = jwt.verify(token, SECRET_KEY, options);
+      decoded = jwt.verify(token, config.SECRET_KEY, options);
       req.isAuthenticated = true;
       req.decoded = decoded;
       next();
