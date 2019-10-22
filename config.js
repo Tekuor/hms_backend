ab5ffe5d47393a704e1493ee1e0157878af5df4f
matekuor <matekuor94@gmail.com>
2019-10-22 16:47:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 const SECRET_KEY = "secretkey23456";
+const port = process.env.PORT || 4000;
+
 
 module.exports = {
   SECRET_KEY: SECRET_KEY,
   db: {
-    url: "mongodb://user1:hospitalUser1@ds135068.mlab.com:35068/hospital_system"
-  }
+    url: "mongodb://localhost:27017/gifty"
+  },
+  port:port
 };
-
-//mongodb://user1:hospitalUser1@ds135068.mlab.com:35068/hospital_system
-
-//mongodb://localhost:27017/gifty
